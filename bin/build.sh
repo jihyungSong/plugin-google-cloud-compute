1a63e7c6f03cd321273aada0c6a74ad67778af69
Jihyung Song <bluese@megazone.com>
2020-08-25 18:13:41 +0900

    Modify build push script

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Build a docker image
 cd ..
-docker build -t pyengine/aws-ec2 .
-# docker tag pyengine/aws-compute_engine pyengine/aws-compute_engine:1.4
-docker tag pyengine/aws-ec2 spaceone/aws-ec2:1.3
+docker build -t pyengine/googlecloud-compute .
+docker tag pyengine/googlecloud-compute pyengine/googlecloud-compute:1.0
+docker tag pyengine/googlecloud-compute spaceone/googlecloud-compute:1.0
