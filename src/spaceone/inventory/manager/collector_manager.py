b2204c1034c4ad9449a43d6c2ef803e308875aa9
Dong Yoo Kang(Xellos00) <xellos.kang@gmail.com>
2020-11-04 17:30:24 +0900

    Update Region name in format of - Country (City) Added Tags to include latitude and longitude

--- src/spaceone/inventory/manager/collector_manager.py
+++ src/spaceone/inventory/manager/collector_manager.py
@@ -101,13 +101,15 @@ class CollectorManager(BaseManager):
 
         server_data = vm_instance_manager.get_server_info(instance, instance_types, disks, zone_info, public_images)
         auto_scaler_vo = auto_scaler_manager.get_auto_scaler_info(instance, instance_group, auto_scaler)
-        load_balancer_vos = lb_manager.get_load_balancer_info(instance, instance_group, backend_svcs, url_maps, target_pools, forwarding_rules)
+        load_balancer_vos = lb_manager.get_load_balancer_info(instance, instance_group, backend_svcs, url_maps,
+                                                              target_pools, forwarding_rules)
         disk_vos = disk_manager.get_disk_info(instance, disks)
         vpc_vo, subnet_vo = vpc_manager.get_vpc_info(instance, vpcs, subnets)
         nic_vos = nic_manager.get_nic_info(instance, subnet_vo)
         security_group_vos = security_group_manager.get_security_group_rules_info(instance, firewalls)
-        security_groups = [d.get('security_group_name') for d in security_group_vos if d.get('security_group_name', '') != '']
-        
+        security_groups = [d.get('security_group_name') for d in security_group_vos if
+                           d.get('security_group_name', '') != '']
+
         server_data.update({
             'nics': nic_vos,
             'disks': disk_vos,
@@ -208,7 +210,8 @@ class CollectorManager(BaseManager):
         ]
 
         for public_image in public_image_list:
-            image_list = self.gcp_connector.list_public_images(project=public_image.get('value'), orderBy='creationTimestamp desc')
+            image_list = self.gcp_connector.list_public_images(project=public_image.get('value'),
+                                                               orderBy='creationTimestamp desc')
             public_images[public_image.get('key')] = image_list
         return public_images
 
@@ -284,30 +287,30 @@ class CollectorManager(BaseManager):
     @staticmethod
     def get_region_from_result(result):
         REGION_INFO = {
-            'asia-east1': {'name': 'Taiwan (Changhua County)', 'tags': {'latitude': '43.8345', 'longitude': '2.1972'}},
-            'asia-east2': {'name': 'Hong Kong', 'tags': {'latitude': '43.8345', 'longitude': '2.1972'}},
-            'asia-northeast1': {'name': 'Japan (Tokyo)', 'tags': {'latitude': '43.8345', 'longitude': '2.1972'}},
-            'asia-northeast2': {'name': 'Japan (Osaka)'},
-            'asia-northeast3': {'name': 'South Korea (Seoul)'},
-            'asia-south1': {'name': 'India (Mumbai)'},
-            'asia-southeast1': {'name': 'Singapore (Jurong West)'},
-            'asia-southeast2': {'name': 'Indonesia (Jakarta)'},
-            'australia-southeast1': {'name': 'Australia (Sydney)'},
-            'europe-north1': {'name': 'Finland (Hamina)'},
-            'europe-west1': {'name': 'Belgium (St. Ghislain)'},
-            'europe-west2': {'name': 'England, UK (London)'},
-            'europe-west3': {'name': 'Germany (Frankfurt)'},
-            'europe-west4': {'name': 'Netherlands (Eemshaven)'},
-            'europe-west6': {'name': 'Switzerland (Zürich)'},
-            'northamerica-northeast1': {'name': 'Canada (Montréal, Québec)'},
-            'southamerica-east1': {'name': 'Brazil (Osasco, São Paulo)'},
-            'us-central1': {'name': 'USA (Council Bluffs, Iowa,)'},
-            'us-east1': {'name': 'US, South Carolina (Moncks Corner)'},
-            'us-east4': {'name': 'US, Northern Virginia (Ashburn)'},
-            'us-west1': {'name': 'US, Oregon (The Dalles, )'},
-            'us-west2': {'name': 'Los Angeles, California, USA'},
-            'us-west3': {'name': 'Salt Lake City, Utah, USA'},
-            'us-west4': {'name': 'Las Vegas, Nevada, USA'},
+            "asia-east1": {"name": "Taiwan (Changhua County)", "tags": {"latitude": "24.051196", "longitude": "120.516430"}},
+            "asia-east2": {"name": "Hong Kong", "tags": {"latitude": "22.283289", "longitude": "114.155851"}},
+            "asia-northeast1": {"name": "Japan (Tokyo)", "tags": {"latitude": "35.660648", "longitude": "139.729186"}},
+            "asia-northeast2": {"name": "Japan (Osaka)", "tags": {"latitude": "34.705403", "longitude": "135.490119"}},
+            "asia-northeast3": {"name": "South Korea (Seoul)", "tags": {"latitude": "37.499968", "longitude": "127.036376"}},
+            "asia-south1": {"name": "India (Mumbai)", "tags": {"latitude": "19.164951", "longitude": "72.851765"}},
+            "asia-southeast1": {"name": "Singapore (Jurong West)", "tags": {"latitude": "1.351376", "longitude": "103.709574"}},
+            "asia-southeast2": {"name": "Indonesia (Jakarta)", "tags": {"latitude": "-6.227851", "longitude": "106.808169"}},
+            "australia-southeast1": {"name": "Australia (Sydney)", "tags": {"latitude": "-33.733694", "longitude": "150.969840"}},
+            "europe-north1": {"name": "Finland (Hamina)", "tags": {"latitude": "60.539504", "longitude": "27.113819"}},
+            "europe-west1": {"name": "Belgium (St.Ghislain)", "tags": {"latitude": "50.471248", "longitude": "3.825493"}},
+            "europe-west2": {"name": "England, UK (London)", "tags": {"latitude": "51.515998", "longitude": "-0.126918"}},
+            "europe-west3": {"name": "Germany (Frankfurt)", "tags": {"latitude": "50.115963", "longitude": "8.669625"}},
+            "europe-west4": {"name": "Netherlands (Eemshaven)", "tags": {"latitude": "53.427625", "longitude": "6.865703"}},
+            "europe-west6": {"name": "Switzerland (Zürich)", "tags": {"latitude": "47.365663", "longitude": "8.524881"}},
+            "northamerica-northeast1": {"name": "Canada, Québec (Montréal)", "tags": {"latitude": "45.501926", "longitude": "-73.570086"}},
+            "southamerica-east1": {"name": "Brazil, São Paulo (Osasco)", "tags": {"latitude": "43.8345", "longitude": "2.1972"}},
+            "us-central1": {"name": "US, Iowa (Council Bluffs)", "tags": {"latitude": "41.221419", "longitude": "-95.862676"}},
+            "us-east1": {"name": "US, South Carolina (Moncks Corner)", "tags": {"latitude": "33.203394", "longitude": "-79.986329"}},
+            "us-east4": {"name": "US, Northern Virginia (Ashburn)", "tags": {"latitude": "39.021075", "longitude": "-77.463569"}},
+            "us-west1": {"name": "US, Oregon (The Dalles)", "tags": {"latitude": "45.631800", "longitude": "-121.200921"}},
+            "us-west2": {"name": "US, California (Los Angeles)", "tags": {"latitude": "34.049329", "longitude": "-118.255265"}},
+            "us-west3": {"name": "US, Utah (Salt Lake City)", "tags": {"latitude": "40.730109", "longitude": "-111.951386"}},
+            "us-west4": {"name": "US, Nevada (Las Vegas)", "tags": {"latitude": "36.092498", "longitude": "-115.086073"}}
         }
 
         match_region_info = REGION_INFO.get(result.get('region_code'))
@@ -321,4 +324,3 @@ class CollectorManager(BaseManager):
             return Region(region_info, strict=False)
 
         return None
-
